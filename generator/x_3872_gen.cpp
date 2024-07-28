fbf7162e8e45a9228e45a6e3535c4373b402e972
Serhat Istin <istins@gmail.com>
2024-07-28 17:06:22 +0300

    Fixed B-meson selection. Some factorizations. Added more histograms

--- generator/x_3872_gen.cpp
+++ generator/x_3872_gen.cpp
@@ -7,93 +7,68 @@
 
 #include <algorithm>
 #include <fmt/ranges.h>
-#include <fmt/std.h>
-#include <string>
+#include <format>
+#include <ranges>
 #include <tuple>
-#include <unordered_map>
-#include <variant>
 
 #include "Pythia8/Pythia.h"
-#include "TFile.h"
-#include "TH1F.h"
-#include "TH2F.h"
-#include <Math/Vector4D.h>
-
-using namespace Pythia8;
-using FourMom = ROOT::Math::PtEtaPhiEVector;
-
-class HistogramRegistry {
-  using Histogram = std::variant<TH1F, TH2F>;
-  std::unordered_map<std::string, Histogram> m_histo_list;
-
-public:
-  template <typename K, typename... T> void Book(K key, T &&...args) {
-    if constexpr (sizeof...(args) == 4) {
-      m_histo_list[key] = TH1F(key, std::forward<T>(args)...);
-    } else if constexpr (sizeof...(args) == 7) {
-      m_histo_list[key] = TH2F(key, std::forward<T>(args)...);
-    } else
-      static_assert(false, "histogram book error ... ");
-  }
-
-  template <typename... T> void Fill(const std::string &key, T... val) {
-    if (m_histo_list.find(key) == m_histo_list.end()) {
-      throw std::runtime_error(fmt::format("{} not booked ? ", key));
-    }
-    if constexpr (sizeof...(val) == 1) {
-      std::get<TH1F>(m_histo_list[key]).Fill(val...);
-    } else if constexpr (sizeof...(val) == 2) {
-      std::get<TH2F>(m_histo_list[key]).Fill(val...);
-    } else {
-      static_assert(false, "histogram fill error... : ");
-    }
-  }
-
-  void Write(const char *fname) {
-    auto *f = TFile::Open(fname, "RECREATE");
-    for (const auto &[k, v] : m_histo_list) {
-      std::visit([](auto &el) { el.Write(); }, v);
-    }
-    f->Close();
-  }
-};
+#include "include/HistogramRegistry.h"
 
 enum pdgId : int {
   Bplus = 521,
-  Bminus = -521,
   Kplus = 321,
-  Kminus = -321,
   X3872 = 9120443,
   Pi0 = 111,
   Pi_plus = 211,
-  Pi_minus = -211,
   Photon = 22,
   Psi = 443,
   Psi2S = 100443,
   D0 = 421,
-  D0_bar = -421,
   Dplus = 411,
-  Dminus = -411
 };
 
-// pick the last particle from the record as in Section-4 here :
-// https://pythia.org/download/pdf/worksheet8183.pdf
-
-auto find_particle = [](const auto *particles, int id) {
-  auto rit =
-      std::find_if(particles->rbegin(), particles->rend(),
-                   [&id](const auto &p) { return std::abs(p.id()) == id; });
-  return std::tuple{rit, rit == particles->rend() ? false : true};
+template <typename F, typename P = Pythia8::Particle>
+concept ParticlePredicate = requires(F& predicate, P ptcl) {
+  { predicate(ptcl) } -> std::same_as<bool>;
 };
 
-auto print_daughters = [](const auto &record, const auto &ptcl,
+template <ParticlePredicate P>
+auto filter_particles(const auto *record, P pred)
+
+{
+  std::vector<Pythia8::Particle> particles(record->size());
+  auto it =
+      std::copy_if(record->cbegin(), record->cend(), particles.begin(), pred);
+  particles.resize(std::distance(particles.begin(), it));
+  return particles;
+};
+
+template <ParticlePredicate P>
+auto filter_indices(const auto *record, P pred)
+
+{
+  std::vector<size_t> indices(4);
+  for (const auto &particle : *record) {
+    if (pred(particle)) {
+      indices.push_back(particle.index());
+    }
+  }
+  return indices;
+};
+
+auto print_daughters = [](const auto *record, const auto &ptcl,
                           bool all = false) {
   const auto &daughters =
       all ? ptcl.daughterListRecursive() : ptcl.daughterList();
-  std::vector<std::string> names(daughters.size());
+  std::vector<decltype(ptcl.name())> names(daughters.size());
   std::transform(daughters.begin(), daughters.end(), names.begin(),
                  [&record](int i) { return (*record)[i].name(); });
-  fmt::print("{} -> {}\n", ptcl.name(), names);
+  fmt::println("{} -> {}", ptcl.name(), names);
+};
+
+auto is_b_meson = [](const auto &p) {
+  return std::abs(p.id()) == pdgId::Bplus and
+         (p.status() == -83 or p.status() == -84);
 };
 
 int main() {
@@ -109,6 +84,7 @@ int main() {
   hists.Book("h_B_eta", "B meson", 100, -3, 3);
   hists.Book("h_B_phi", "B meson", 100, -4, 4);
   hists.Book("h_B_E", "B meson", 100, 0, 150);
+  hists.Book("h_B_N", "Bmeson", 10, 0, 10);
 
   hists.Book("h_K_pt", "Kaon", 100, 0, 100);
   hists.Book("h_K_eta", "Kaon", 100, -3, 3);
@@ -120,7 +96,17 @@ int main() {
   hists.Book("h_X3872_phi", "X(3872)", 100, -4, 4);
   hists.Book("h_X3872_E", "X(3872)", 100, 0, 150);
 
-  Pythia pythia;
+  hists.Book("h_X3872_d1_pt", "X(3872) daughter1", 100, 0, 100);
+  hists.Book("h_X3872_d1_eta", "X(3872) daughter1", 100, -3, 3);
+  hists.Book("h_X3872_d1_phi", "X(3872) daughter1", 100, -4, 4);
+  hists.Book("h_X3872_d1_E", "X(3872) daughter1", 100, 0, 150);
+
+  hists.Book("h_X3872_d2_pt", "X(3872) daughter2", 100, 0, 100);
+  hists.Book("h_X3872_d2_eta", "X(3872) daughter2", 100, -3, 3);
+  hists.Book("h_X3872_d2_phi", "X(3872) daughter2", 100, -4, 4);
+  hists.Book("h_X3872_d2_E", "X(3872) daughter2", 100, 0, 150);
+
+  Pythia8::Pythia pythia;
   // Add X(3872) meson to Pythia's database or something ...
   pythia.particleData.addParticle(9120443, "X_3872", "X_3872_bar", 3, 0, 0,
                                   3.87169, 0.00122, 0, 0, 0);
@@ -138,35 +124,32 @@ int main() {
     }
 
     const auto *record = pythia.event.particles();
-    // shortly if a B meson is found ...
-    if (const auto &[B, found] = find_particle(record, pdgId::Bplus); found) {
-      auto x3872 = record->at(B->daughter1());
-      auto kaon = record->at(B->daughter2());
+    const auto &b_mesons = filter_particles(record, is_b_meson);
+
+    for (const auto &B : b_mesons) {
+      auto kaon = record->at(B.daughter1());
+      auto x3872 = record->at(B.daughter2());
       if (std::abs(x3872.id()) != pdgId::X3872) {
-        std::swap(x3872, kaon);
+        std::swap(kaon, x3872);
       }
-
-      hists.Fill("h_B_pt", B->pT());
-      hists.Fill("h_B_eta", B->eta());
-      hists.Fill("h_B_phi", B->phi());
-      hists.Fill("h_B_E", B->e());
-
-      hists.Fill("h_K_pt", kaon.pT());
-      hists.Fill("h_K_eta", kaon.eta());
-      hists.Fill("h_K_phi", kaon.phi());
-      hists.Fill("h_K_E", kaon.e());
+      hists.Fill("h_B_pt", B.pT());
+      hists.Fill("h_B_eta", B.eta());
+      hists.Fill("h_B_phi", B.phi());
+      hists.Fill("h_B_E", B.e());
+      hists.Fill("h_B_N", b_mesons.size());
 
       hists.Fill("h_X3872_pt", x3872.pT());
       hists.Fill("h_X3872_eta", x3872.eta());
       hists.Fill("h_X3872_phi", x3872.phi());
       hists.Fill("h_X3872_E", x3872.e());
 
+      hists.Fill("h_K_pt", kaon.pT());
+      hists.Fill("h_K_eta", kaon.eta());
+      hists.Fill("h_K_phi", kaon.phi());
+      hists.Fill("h_K_E", kaon.e());
 
-#ifdef DEBUG_GEN
-      fmt::print("{} -> {} + {} \n ", B->name(), x3872.name(), kaon.name());
-      print_daughters(record, kaon);
-      print_daughters(record, x3872, true);
-#endif
+      const auto &x3872_d1 = record->at(x3872.daughter1());
+      const auto &x3872_d2 = record->at(x3872.daughter2());
     }
 
     // filter all final particles, fill some histograms  ...:
