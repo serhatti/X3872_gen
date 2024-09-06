edc2eca36634c3db914198ff93786b7b31719233
Serhat Istin <istins@gmail.com>
2024-09-06 20:43:43 +0300

    use type traits in histogram booking

--- generator/include/HistogramRegistry.h
+++ generator/include/HistogramRegistry.h
@@ -3,19 +3,17 @@
           istin@cern.ch
 */
 
-
 #ifndef HISTOGRAMREGISTRY_H
 #define HISTOGRAMREGISTRY_H
 
-
+#include <string>
+#include <type_traits>
 #include <unordered_map>
 #include <variant>
-#include <string>
 
 #include "TFile.h"
 #include "TH1F.h"
 #include "TH2F.h"
-
 
 class HistogramRegistry {
   using Histogram = std::variant<TH1F, TH2F>;
@@ -23,19 +21,19 @@ class HistogramRegistry {
   std::vector<std::string> m_keys;
 
 public:
-  template <typename K, typename... T> void Book(const K& key, T &&...args) {
-    if constexpr (sizeof...(args) == 4) {
+  template <typename K, typename... T> void Book(const K &key, T &&...args) {
+    if constexpr (std::is_constructible_v<TH1F, K, T...>) {
       m_histo_list[key] = TH1F(key, std::forward<T>(args)...);
-    } else if constexpr (sizeof...(args) == 7) {
+    } else if constexpr (std::is_constructible_v<TH2F, K, T...>) {
       m_histo_list[key] = TH2F(key, std::forward<T>(args)...);
     } else
       static_assert(false, "histogram book error ... ");
-      m_keys.push_back(key);
+    m_keys.push_back(key);
   }
 
   template <typename... T> void Fill(const std::string &key, T... val) {
-    if(m_histo_list.find(key) == m_histo_list.end()){
-        throw std::runtime_error(std::format("{} not boked ?",key));
+    if (m_histo_list.find(key) == m_histo_list.end()) {
+      throw std::runtime_error(std::format("{} not boked ?", key));
     }
 
     if constexpr (sizeof...(val) == 1) {
@@ -48,14 +46,13 @@ public:
   }
 
   void Write(const char *fname) {
-    std::sort(m_keys.begin(),m_keys.end());
+    std::sort(m_keys.begin(), m_keys.end());
     auto *f = TFile::Open(fname, "RECREATE");
-    for (const auto & k : m_keys) {
+    for (const auto &k : m_keys) {
       std::visit([](auto &el) { el.Write(); }, m_histo_list[k]);
     }
     f->Close();
   }
 };
-
 
 #endif
